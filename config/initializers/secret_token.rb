0a4a25cb5f24dc500a65a2e6a9a13a71c684bcaf
Halsey <chx2103@gmail.com>
2012-08-06 17:38:56 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatonline::Application.config.secret_token = 'f845beddc9bafc74e960d39c207a2cbf5734940a4c8745ca90b51e6fb2e8de790492922cbc8a73e19edfd42868ff355998835e99d73d54d780b41b58828c288e'
