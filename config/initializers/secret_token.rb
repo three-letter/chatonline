0c9b62d1b74d0c1d5b1d7425964492a22873f1da
Halsey <lyu302@gmail.com>
2012-08-06 23:15:11 +0800

    init chatonline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatonline::Application.config.secret_token = 'c60debf067afc7f92f1ef3e7a6b68fbc58e1a81617ada79fe076e8205604d0dd3f37b565d6dbe347406ac9ce5fb83d4709a89e7318be3ddbe793e32f5ab75b5e'
